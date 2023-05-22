6dee16246678804a1fc1ae7a4d4c751f59258528
diomferreira <diomferreira@gmail.com>
2023-05-22 20:39:17 +0100

    ""

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -97,14 +97,81 @@ class CompanyController extends Controller
         $ativo_nao_corrente = AtivoNaoCorrente::where('ativo_id', $ativo->ativo_id)->first();
         $passivo_corrente = PassivoCorrente::where('passivo_id', $passivo->passivo_id)->first();
         $passivo_nao_corrente = PassivoNaoCorrente::where('passivo_id', $passivo->passivo_id)->first();
-        
+
+        // Set a circular graph to see how much of the company is owned by the user and how much is owned by bank or other investors
+        $total_capital_proprio = $capital_proprio->capitaisproprios_capitalrealizado + $capital_proprio->capitaisproprios_outrosinstrumentoscapitalproprio + $capital_proprio->capitaisproprios_reservaslegais + $capital_proprio->capital_proprio_resultados_transitados + $capital_proprio->capitaisproprios_resultadostransitados + $capital_proprio->capitaisproprios_outrasvariacoescapitalproprio;
+        $total_passivo_corrente = $passivo_corrente->passivoscorrentes_fornecedores + $passivo_corrente->passivoscorrentes_estadoeoutrosentespublicos + $passivo_corrente->passivoscorrentes_accionistas_socios + $passivo_corrente->passivoscorrentes_financiamentosobtidos + $passivo_corrente->passivoscorrentes_outrascontasapagar + $passivo_corrente->passivoscorrentes_outros;
+
+        // Set the percentage of the company owned by the user
+        $percentage_user = ($total_capital_proprio - $total_passivo_corrente) / $total_capital_proprio * 100;
+        $percentage_bank = $total_passivo_corrente / $total_capital_proprio * 100;
+
+        // Set the graph using graph.js
+        $dataPoints = array(
+            array("label"=> "User", "y"=> $percentage_user),
+            array("label"=> "Bank", "y"=> $percentage_bank)
+        );
+
+        $total_ativo_corrente = ( 
+        $ativo_corrente->ativoscorrentes_inventarios +
+        $ativo_corrente->ativoscorrentes_clientes + 
+        $ativo_corrente->ativoscorrentes_adiantamentosafornecedores + 
+        $ativo_corrente->ativoscorrentes_estadoeoutrosentespublicos + 
+        $ativo_corrente->ativoscorrentes_outrascontasareceber + 
+        $ativo_corrente->ativoscorrentes_diferimentos +
+        $ativo_corrente->ativoscorrentes_outrosativoscorrentes + 
+        $ativo_corrente->ativoscorrentes_caixaedepositosbancarios);
+
+        $total_passivo_corrente = ( 
+        $passivo_corrente->passivoscorrentes_fornecedores + 
+        $passivo_corrente->passivoscorrentes_estadoeoutrosentespublicos + 
+        $passivo_corrente->passivoscorrentes_accionistas_socios + 
+        $passivo_corrente->passivoscorrentes_financiamentosobtidos + 
+        $passivo_corrente->passivoscorrentes_outrascontasapagar + 
+        $passivo_corrente->passivoscorrentes_outros );
+
+        $total_capital_proprio = (
+        $capital_proprio->capitaisproprios_capitalrealizado +
+        $capital_proprio->capitaisproprios_outrosinstrumentoscapitalproprio +
+        $capital_proprio->capitaisproprios_reservaslegais +
+        $capital_proprio->capitaisproprios_resultadostransitados +
+        $capital_proprio->capitaisproprios_outrasvariacoescapitalproprio + 
+        $result->resultado_liquido
+        );
+
+        $total_ativo_nao_corrente = (
+        $ativo_nao_corrente->ativonaocorrente_ativofixo +
+        $ativo_nao_corrente->ativonaocorrente_goodwill +
+        $ativo_nao_corrente->ativonaocorrente_ativointangivel +
+        $ativo_nao_corrente->ativonaocorrente_outros );
+        // Ratio liquidez geral = ativo corrente/passivo corrente
+        $ratio_liquidez_geral = $total_ativo_corrente / $total_passivo_corrente * 100;
+        $ratio_liquidez_geral = round($ratio_liquidez_geral, 2) . '%';
+
+        // Ratio liquidez reduzida = (ativo corrente - inventários - act.biologicos) / passivo corrente
+        $ratio_liquidez_reduzida = ($total_ativo_corrente - $ativo_corrente->ativoscorrentes_inventarios - $ativo_corrente->ativoscorrentes_activosbiologicos) / $total_passivo_corrente * 100;
+        $ratio_liquidez_reduzida = round($ratio_liquidez_reduzida, 2) . '%';
+
+        // Ratio autonomia financeira = capital proprio /  ativo total
+        $ratio_autonomia_financeira = $total_capital_proprio / ($total_ativo_corrente + $total_ativo_nao_corrente) * 100;
+        $ratio_autonomia_financeira = round($ratio_autonomia_financeira, 2) . '%';
+
+        // Endividamento = passivo total / ativo total
+
+        $ratio_endividamento = ($total_passivo_corrente + $passivo_nao_corrente->passivonaocorrente_passivofinanceiro) / ($total_ativo_corrente + $total_ativo_nao_corrente) * 100;
+        $ratio_endividamento = round($ratio_endividamento, 2) . '%';
+
+        dd($ratio_endividamento);
+
         
 
         return view('dashboard', 
         [
         'company' => $company,
         'user' => $user,
-        'report' => $report
+        'report' => $report,
+        'dataPoints' => $dataPoints,
+        'ratio_liquidez_geral' => $ratio_liquidez_geral
         ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,14 +23,17 @@
         </form>
     </div>
 </div>    
+
+<h1 class="text-uppercase text-primary">Ratios</h1>
+<hr class="hr-primary mb-4">
 <div class="row">
     <div class="col-md-6 col-xl-3 mb-4">
         <div class="card shadow border-start-primary py-2">
             <div class="card-body">
                 <div class="row align-items-center no-gutters">
                     <div class="col me-2">
-                        <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span>Earnings (monthly)</span></div>
-                        <div class="text-dark fw-bold h5 mb-0"><span>€{{ $report->balance->capitalProprio->capitaisproprios_capitalrealizado + 1 }}</span></div>
+                        <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span>Liquidez Corrente</span></div>
+                        <div class="text-dark fw-bold h5 mb-0"><span>€{{ $report->balance->capitalProprio->capitaisproprios_capitalrealizado}}</span></div>
                     </div>
                     <div class="col-auto"><i class="fas fa-calendar fa-2x text-gray-300"></i></div>
                 </div>
@@ -86,6 +89,97 @@
         </div>
     </div>
 </div>
+<hr>
+
+
+<div class="col-lg-5 col-xl-4">
+    <div class="card shadow mb-4">
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <h6 class="text-primary fw-bold m-0">Revenue Sources</h6>
+            <div class="dropdown no-arrow"><button class="btn btn-link btn-sm dropdown-toggle" aria-expanded="false" data-bs-toggle="dropdown" type="button"><i class="fas fa-ellipsis-v text-gray-400"></i></button>
+                <div class="dropdown-menu shadow dropdown-menu-end animated--fade-in">
+                    <p class="text-center dropdown-header">Action Options:</p><a class="dropdown-item" href="#">&nbsp;Concept</a><a class="dropdown-item" href="#">&nbsp;Interpretation</a>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="chart-area"><canvas id="graphCanvas"></canvas></div>
+            <div class="text-center small mt-4">
+                <?php foreach ($dataPoints as $dataPoint): ?>
+                    <span class="me-2">
+                        <i class="fas fa-circle <?php echo $dataPoint['label'] === 'User' ? 'text-primary' : 'text-success'; ?>"></i>
+                        &nbsp;<?php echo $dataPoint['label']; ?>
+                    </span>
+                <?php endforeach; ?>
+            </div>                        
+        </div>
+    </div>
+</div>
+
+{{-- <div class="row">
+    <div class="col-md-6 col-xl-3 mb-4">
+        <div class="card shadow border-start-primary py-2">
+            <div class="card-body">
+                <div class="row align-items-center no-gutters">
+                    <div class="col me-2">
+                        <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span>Earnings (monthly)</span></div>
+                        <div class="text-dark fw-bold h5 mb-0"><span>€{{ $report->balance->capitalProprio->capitaisproprios_capitalrealizado}}</span></div>
+                    </div>
+                    <div class="col-auto"><i class="fas fa-calendar fa-2x text-gray-300"></i></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6 col-xl-3 mb-4">
+        <div class="card shadow border-start-success py-2">
+            <div class="card-body">
+                <div class="row align-items-center no-gutters">
+                    <div class="col me-2">
+                        <div class="text-uppercase text-success fw-bold text-xs mb-1"><span>Earnings (annual)</span></div>
+                        <div class="text-dark fw-bold h5 mb-0"><span>€215,000</span></div>
+                    </div>
+                    <div class="col-auto"><i class="fas fa-dollar-sign fa-2x text-gray-300"></i></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6 col-xl-3 mb-4">
+        <div class="card shadow border-start-info py-2">
+            <div class="card-body">
+                <div class="row align-items-center no-gutters">
+                    <div class="col me-2">
+                        <div class="text-uppercase text-info fw-bold text-xs mb-1"><span>Tasks</span></div>
+                        <div class="row g-0 align-items-center">
+                            <div class="col-auto">
+                                <div class="text-dark fw-bold h5 mb-0 me-3"><span>50%</span></div>
+                            </div>
+                            <div class="col">
+                                <div class="progress progress-sm">
+                                    <div class="progress-bar bg-info" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="visually-hidden">50%</span></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-auto"><i class="fas fa-clipboard-list fa-2x text-gray-300"></i></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6 col-xl-3 mb-4">
+        <div class="card shadow border-start-warning py-2">
+            <div class="card-body">
+                <div class="row align-items-center no-gutters">
+                    <div class="col me-2">
+                        <div class="text-uppercase text-warning fw-bold text-xs mb-1"><span>Pending Requests</span></div>
+                        <div class="text-dark fw-bold h5 mb-0"><span>18</span></div>
+                    </div>
+                    <div class="col-auto"><i class="fas fa-comments fa-2x text-gray-300"></i></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
 <div class="row">
     <div class="col-lg-7 col-xl-8">
         <div class="card shadow mb-4">
@@ -120,127 +214,7 @@
             </div>
         </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-lg-6 mb-4">
-        <div class="card shadow mb-4">
-            <div class="card-header py-3">
-                <h6 class="text-primary fw-bold m-0">Projects</h6>
-            </div>
-            <div class="card-body">
-                <h4 class="small fw-bold">Server migration<span class="float-end">20%</span></h4>
-                <div class="progress mb-4">
-                    <div class="progress-bar bg-danger" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%;"><span class="visually-hidden">20%</span></div>
-                </div>
-                <h4 class="small fw-bold">Sales tracking<span class="float-end">40%</span></h4>
-                <div class="progress mb-4">
-                    <div class="progress-bar bg-warning" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;"><span class="visually-hidden">40%</span></div>
-                </div>
-                <h4 class="small fw-bold">Customer Database<span class="float-end">60%</span></h4>
-                <div class="progress mb-4">
-                    <div class="progress-bar bg-primary" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"><span class="visually-hidden">60%</span></div>
-                </div>
-                <h4 class="small fw-bold">Payout Details<span class="float-end">80%</span></h4>
-                <div class="progress mb-4">
-                    <div class="progress-bar bg-info" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"><span class="visually-hidden">80%</span></div>
-                </div>
-                <h4 class="small fw-bold">Account setup<span class="float-end">Complete!</span></h4>
-                <div class="progress mb-4">
-                    <div class="progress-bar bg-success" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"><span class="visually-hidden">100%</span></div>
-                </div>
-            </div>
-        </div>
-        <div class="card shadow mb-4">
-            <div class="card-header py-3">
-                <h6 class="text-primary fw-bold m-0">Todo List</h6>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">
-                    <div class="row align-items-center no-gutters">
-                        <div class="col me-2">
-                            <h6 class="mb-0"><strong>Lunch meeting</strong></h6><span class="text-xs">10:30 AM</span>
-                        </div>
-                        <div class="col-auto">
-                            <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1"></label></div>
-                        </div>
-                    </div>
-                </li>
-                <li class="list-group-item">
-                    <div class="row align-items-center no-gutters">
-                        <div class="col me-2">
-                            <h6 class="mb-0"><strong>Lunch meeting</strong></h6><span class="text-xs">11:30 AM</span>
-                        </div>
-                        <div class="col-auto">
-                            <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-2"><label class="form-check-label" for="formCheck-2"></label></div>
-                        </div>
-                    </div>
-                </li>
-                <li class="list-group-item">
-                    <div class="row align-items-center no-gutters">
-                        <div class="col me-2">
-                            <h6 class="mb-0"><strong>Lunch meeting</strong></h6><span class="text-xs">12:30 AM</span>
-                        </div>
-                        <div class="col-auto">
-                            <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-3"><label class="form-check-label" for="formCheck-3"></label></div>
-                        </div>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </div>
-    <div class="col">
-        <div class="row">
-            <div class="col-lg-6 mb-4">
-                <div class="card text-white bg-primary shadow">
-                    <div class="card-body">
-                        <p class="m-0">Primary</p>
-                        <p class="text-white-50 small m-0">#4e73df</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6 mb-4">
-                <div class="card text-white bg-success shadow">
-                    <div class="card-body">
-                        <p class="m-0">Success</p>
-                        <p class="text-white-50 small m-0">#1cc88a</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6 mb-4">
-                <div class="card text-white bg-info shadow">
-                    <div class="card-body">
-                        <p class="m-0">Info</p>
-                        <p class="text-white-50 small m-0">#36b9cc</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6 mb-4">
-                <div class="card text-white bg-warning shadow">
-                    <div class="card-body">
-                        <p class="m-0">Warning</p>
-                        <p class="text-white-50 small m-0">#f6c23e</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6 mb-4">
-                <div class="card text-white bg-danger shadow">
-                    <div class="card-body">
-                        <p class="m-0">Danger</p>
-                        <p class="text-white-50 small m-0">#e74a3b</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6 mb-4">
-                <div class="card text-white bg-secondary shadow">
-                    <div class="card-body">
-                        <p class="m-0">Secondary</p>
-                        <p class="text-white-50 small m-0">#858796</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+</div> --}}
 
 @component('_components.cardModal' , [
     'id' => 'modal-option',
@@ -291,5 +265,49 @@
         window.location.href = "{{ url('/') }}" + "/dashboard/" + company_id + "/reports/" + year;
     }); 
 
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var dataPoints = <?php echo json_encode($dataPoints); ?>;
+
+        function setupGraph(dataPoints) {
+        var ctx = document.getElementById('graphCanvas').getContext('2d');
+
+        var chart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: dataPoints.map(function (dataPoint) {
+                return dataPoint.label + ': ';
+            }),
+            datasets: [{
+            label: '',
+            backgroundColor: ['rgba(78, 115, 223, 0.8)', 'rgba(28, 200, 138, 0.8)', 'rgba(54, 185, 204, 0.8)'],
+            borderColor: ['#ffffff', '#ffffff', '#ffffff'],
+            data: dataPoints.map(function (dataPoint) {
+                return parseFloat(dataPoint.y).toFixed(2);
+            }),
+            }],
+        },
+        options: {
+            maintainAspectRatio: false,
+            legend: {
+            display: false,
+            // make animation smooth
+            animation: {
+                duration: 10000000,
+            },
+            labels: {
+                fontStyle: 'normal',
+            },
+            },
+            title: {
+            fontStyle: 'normal',
+            },
+        },
+        });
+    }
+
+    // Call the setupGraph function with the dataPoints array
+    setupGraph(dataPoints);
+    });
 </script>
 @endsection
